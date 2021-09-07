b77b2c222a1ab5dd09f20ab5727780525afacb86
吴锦辉 <528935881@qq.com>
2021-09-07 15:16:09 +0800

    feat: 提交规范钩子

---
+++ .commitlintrc.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: git提交规范配置
+ * @Author: 吴锦辉
+ * @Date: 2021-09-07 15:12:45
+ * @LastEditTime: 2021-09-07 15:12:46
+ */
+
+module.exports = { extends: ['@commitlint/config-conventional'] };
