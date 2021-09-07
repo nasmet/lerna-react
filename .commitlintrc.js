aeb7482073198ef80026a20fecffa6a35fc17e79
吴锦辉 <528935881@qq.com>
2021-09-07 15:19:45 +0800

    style: 文档

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,7 +2,8 @@
  * @Description: git提交规范配置
  * @Author: 吴锦辉
  * @Date: 2021-09-07 15:12:45
- * @LastEditTime: 2021-09-07 15:12:46
+ * @LastEditTime: 2021-09-07 15:18:48
+ * @Reference https://www.npmjs.com/package/@commitlint/config-conventional
  */
 
 module.exports = { extends: ['@commitlint/config-conventional'] };
