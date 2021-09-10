27ba223f20e637a9a8a9ca8630ee8bbfb7a59a55
吴锦辉 <528935881@qq.com>
2021-09-10 16:52:52 +0800

    fix: 桥接方法

--- packages/wjh-util/src/js-bridge.js
+++ packages/wjh-util/src/js-bridge.js
@@ -2,16 +2,118 @@
  * @Description: 桥接方法，原生h5互调方法
  * @Author: 吴锦辉
  * @Date: 2021-08-30 10:16:44
- * @LastEditTime: 2021-08-30 10:21:18
+ * @LastEditTime: 2021-09-10 15:56:59
  */
 
-/* eslint-disable no-undef */
+(() => {
+  if (window.WebViewJavascriptBridge) {
+    return;
+  }
+
+  const messageHandlers = {};
+  const responseCallbacks = {};
+  let uniqueId = 1;
+
+  function _doSend(message, responseCallback) {
+    if (responseCallback) {
+      uniqueId += 1;
+
+      const callbackId = `cb_${uniqueId}_${new Date().getTime()}`;
+
+      responseCallbacks[callbackId] = responseCallback;
+      message['callbackId'] = callbackId;
+    }
+
+    const msg = JSON.stringify(message || {});
+
+    if (window.WVJBInterface) {
+      window.WVJBInterface.notice(msg);
+    } else {
+      console.warn('没有注入WVJBInterface');
+    }
+  }
+
+  function _dispatchMessageFromJava(message) {
+    let responseCallback;
+
+    if (message.responseId) {
+      responseCallback = responseCallbacks[message.responseId];
+
+      if (!responseCallback) {
+        return;
+      }
+
+      responseCallback(message.responseData);
+      delete responseCallbacks[message.responseId];
+    } else {
+      if (message.callbackId) {
+        const callbackResponseId = message.callbackId;
+
+        responseCallback = responseData => {
+          _doSend({
+            handlerName: message.handlerName,
+            responseId: callbackResponseId,
+            responseData,
+          });
+        };
+      }
+
+      const handler = messageHandlers[message.handlerName];
+
+      if (!handler) {
+        console.warn('no handler for message from java', message);
+      } else {
+        handler(message.data, responseCallback);
+      }
+    }
+  }
+
+  const bridge = {
+    registerHandler(handlerName, handler) {
+      messageHandlers[handlerName] = handler;
+    },
+    callHandler(handlerName, data, responseCallback) {
+      _doSend(
+        {
+          handlerName,
+          data,
+        },
+        responseCallback
+      );
+    },
+    disableJavascriptAlertBoxSafetyTimeout(disable) {
+      this.callHandler('_disableJavascriptAlertBoxSafetyTimeout', disable !== false);
+    },
+    _handleMessageFromJava(messageJSON) {
+      _dispatchMessageFromJava(messageJSON);
+    },
+    hasNativeMethod(name, responseCallback) {
+      this.callHandler('_hasNativeMethod', name, responseCallback);
+    },
+  };
+
+  bridge.registerHandler('_hasJavascriptMethod', (data, responseCallback) => {
+    responseCallback(!!messageHandlers[data]);
+  });
+
+  const callbacks = window.WVJBCallbacks;
+
+  delete window.WVJBCallbacks;
+
+  if (callbacks) {
+    for (let i = 0; i < callbacks.length; i += 1) {
+      callbacks[i](bridge);
+    }
+  }
+
+  window.WebViewJavascriptBridge = bridge;
+})();
 
 export default function initJsBridge() {
   // eslint-disable-next-line consistent-return
   function setupWebViewJavascriptBridge(callback) {
     if (window.WebViewJavascriptBridge) {
-      return callback(WebViewJavascriptBridge);
+      return callback(window.WebViewJavascriptBridge);
     }
 
     if (window.WVJBCallbacks) {
