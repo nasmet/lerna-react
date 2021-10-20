09b6e8078179e5521fd633fced701c9501053923
吴锦辉 <528935881@qq.com>
2021-10-20 11:54:24 +0800

    feat: 使用抽象语法数删除log

--- packages/wjh-tool/bin/ast.js
+++ packages/wjh-tool/bin/ast.js
@@ -4,32 +4,79 @@
  * @Description: 使用ast删除console.log
  * @Author: 吴锦辉
  * @Date: 2021-10-18 10:44:42
- * @LastEditTime: 2021-10-18 10:49:18
+ * @LastEditTime: 2021-10-20 11:52:25
  */
 
-const babylon = require('babylon');
+const fs = require('fs');
+const path = require('path');
+const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const generator = require('@babel/generator').default;
+const { argv } = require('yargs');
 // const types = require('@babel/types');
 const { streamWriteFile, streamReadFile } = require('../utils/stream');
 
+const root = process.cwd();
+
+if (argv.dc) {
+  deleteLog(root);
+
+  return;
+}
+
+console.log('命令参数有误');
+
 /**
- * @description: 通过抽象语法树删除log
- * @param {string} file 文件路径
+ * @description: 通过抽象语法树递归删除log
+ * @param {string} root 根路径
  * @return {void}
  */
-function deleteLog(file) {
+function deleteLog(root) {
+  console.log('批量删除中');
+
   /** Step Parse */
   function parse(code) {
-    return babylon.parse(code);
+    return parser.parse(code, {
+      // parse in strict mode and allow module declarations
+      sourceType: 'module',
+
+      plugins: [
+        // enable jsx and flow syntax
+        'jsx',
+        'flow',
+      ],
+    });
   }
 
   /** Step Transform */
   function transform(ast) {
     traverse(ast, {
       ExpressionStatement(path) {
-        if (path.node.expression.callee.object.name === 'console') {
+        if (
+          path.node.expression.callee &&
+          path.node.expression.callee.object &&
+          path.node.expression.callee.object.name === 'console'
+        ) {
           path.remove();
+
+          return;
+        }
+
+        const { expressions = [] } = path.node.expression;
+
+        let len = expressions.length;
+
+        for (let i = 0; i < len; i += 1) {
+          if (
+            expressions[i].callee &&
+            expressions[i].callee.object &&
+            expressions[i].callee.object.name === 'console'
+          ) {
+            expressions.splice(i, 1);
+
+            len -= 1;
+            i -= 1;
+          }
         }
       },
     });
@@ -40,55 +87,53 @@ function deleteLog(file) {
     return generator(ast).code;
   }
 
-  streamReadFile(file)
-    .then(res => {
-      const ast = parse(res);
+  /** 文件log删除 */
+  function execute(file) {
+    streamReadFile(file)
+      .then(res => {
+        const ast = parse(res);
 
-      transform(ast);
+        transform(ast);
 
-      const targetCode = generate(ast);
+        const targetCode = generate(ast);
 
-      streamWriteFile(file, targetCode).catch(() => {
-        console.error('写入文件失败');
+        streamWriteFile(file, targetCode).catch(e => {
+          console.error(`${file} 写入文件失败: `, e);
+        });
+      })
+      .catch(e => {
+        console.error(`${file} 读取文件失败: `, e);
       });
-    })
-    .catch(() => {
-      console.error('读取文件失败');
-    });
-}
-
-/**
- * @description: 通过抽象语法树添加内容
- * @param {string} file 文件路径
- * @param {string} content
- * @return {void}
- */
-function addContent(file, content) {
-  /** Step Parse */
-  function parse(code) {
-    return babylon.parse(code);
   }
 
-  /** Step Generate */
-  function generate(ast) {
-    return generator(ast).code;
+  /**
+   * @description: 递归遍历
+   * @param {string} root 文件路径
+   * @return {void}
+   */
+  function recursionTraversal(root) {
+    fs.stat(root, (err, stat) => {
+      if (err) {
+        console.error(err);
+      } else if (stat.isFile() && /\.(js|jsx)$/.test(root)) {
+        execute(root);
+      } else if (stat.isDirectory()) {
+        if (/node_modules/.test(root)) {
+          return;
+        }
+
+        fs.readdir(root, (err, files) => {
+          if (err) {
+            console.error(err);
+          } else {
+            files.forEach(file => {
+              recursionTraversal(path.join(root, file));
+            });
+          }
+        });
+      }
+    });
   }
 
-  streamReadFile(file)
-    .then(res => {
-      const ast = parse(res);
-
-      const addAst = parse(content);
-
-      ast.program.body.push(...addAst.program.body);
-
-      const targetCode = generate(ast);
-
-      streamWriteFile(file, targetCode).catch(() => {
-        console.error('写入文件失败');
-      });
-    })
-    .catch(() => {
-      console.error('读取文件失败');
-    });
+  recursionTraversal(root);
 }
