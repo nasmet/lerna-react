77ff936d08aef8d7c13c43758137e002cc873e9c
吴锦辉 <528935881@qq.com>
2021-11-09 15:14:40 +0800

    feat: pm2配置集群

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
  * @Description: pm2配置文件
  * @Author:
  * @Date: 2021-10-09 11:00:04
- * @LastEditTime: 2021-10-09 11:45:53
+ * @LastEditTime: 2021-11-09 14:35:09
  */
 
 module.exports = {
@@ -11,6 +11,11 @@ module.exports = {
       name: 'main',
       script: './packages/wjh-server/src/index.js',
       watch: true,
+      /** 应用启动模式，支持fork和cluster模式 */
+      exec_mode: 'cluster',
+      /** 应用启动实例个数，仅在cluster模式有效 */
+      instances: 4,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         PORT: 3000,
         NODE_ENV: 'development',
@@ -24,6 +29,11 @@ module.exports = {
       name: 'operation',
       script: './packages/wjh-server-operation/src/index.js',
       watch: true,
+      /** 应用启动模式，支持fork和cluster模式 */
+      exec_mode: 'cluster',
+      /** 应用启动实例个数，仅在cluster模式有效 */
+      instances: 4,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         PORT: 3001,
         NODE_ENV: 'development',
@@ -37,6 +47,11 @@ module.exports = {
       name: 'hidemoney',
       script: './packages/wjh-server-hidemoney/src/index.js',
       watch: true,
+      /** 应用启动模式，支持fork和cluster模式 */
+      exec_mode: 'cluster',
+      /** 应用启动实例个数，仅在cluster模式有效 */
+      instances: 4,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         PORT: 3002,
         NODE_ENV: 'development',
