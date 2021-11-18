d978bbd9324e63690283893bd2699356f9f1d6b2
吴锦辉 <528935881@qq.com>
2021-11-18 15:53:52 +0800

    feat: js桥接

--- packages/wjh-util/src/js-bridge.js
+++ packages/wjh-util/src/js-bridge.js
@@ -2,26 +2,40 @@
  * @Description: 桥接方法，原生h5互调方法
  * @Author: 吴锦辉
  * @Date: 2021-08-30 10:16:44
- * @LastEditTime: 2021-09-10 15:56:59
+ * @LastEditTime: 2021-11-18 15:51:44
  */
 
-(() => {
-  if (window.WebViewJavascriptBridge) {
-    return;
+class JsBridge {
+  constructor() {
+    this.methodHandlers = {};
+    this.responseCallbacks = {};
+    this.uniqueId = 1;
   }
 
-  const messageHandlers = {};
-  const responseCallbacks = {};
-  let uniqueId = 1;
+  /**
+   * @description: Native调JS方法的核心
+   * @param {string} methodName 前端与Native协商好的方法名称
+   * @param {function} method 执行的方法
+   * @return {}
+   */
+  registerHandler(methodName, method) {
+    this.methodHandlers[methodName] = method;
+  }
 
-  function _doSend(message, responseCallback) {
+  /**
+   * @description: JS调Native方法的核心
+   * @param {object} message
+   * @param {string} message.messageName 前端与Native协商好的方法名称
+   * @param {object} message.data 参数
+   * @param {function} responseCallback 响应回调
+   * @return {}
+   */
+  callHandler(message, responseCallback) {
     if (responseCallback) {
-      uniqueId += 1;
-
-      const callbackId = `cb_${uniqueId}_${new Date().getTime()}`;
-
-      responseCallbacks[callbackId] = responseCallback;
-      message['callbackId'] = callbackId;
+      // eslint-disable-next-line no-plusplus
+      const responseId = `cb_${this.uniqueId++}_${new Date().getTime()}`;
+      this.responseCallbacks[responseId] = responseCallback;
+      message['responseId'] = responseId;
     }
 
     const msg = JSON.stringify(message || {});
@@ -33,137 +47,100 @@
     }
   }
 
-  function _dispatchMessageFromJava(message) {
-    let responseCallback;
+  /**
+   * @description: app端分发消息
+   * @param {object} message
+   * @param {string} message.responseId
+   * @param {string} message.responseData
+   * @return {string}
+   */
+  _dispatchMessageFromAPP(message) {
+    const { responseId, responseData, methodName, data } = message;
 
-    if (message.responseId) {
-      responseCallback = responseCallbacks[message.responseId];
+    const responseCallback = this.responseCallbacks[responseId];
 
+    /** JS调原生回调 */
+    if (responseId) {
       if (!responseCallback) {
+        console.warn(`responseId ${responseId} 不存在`);
+
         return;
       }
 
-      responseCallback(message.responseData);
-      delete responseCallbacks[message.responseId];
-    } else {
-      if (message.callbackId) {
-        const callbackResponseId = message.callbackId;
+      Reflect.deleteProperty(this.responseCallbacks, responseId);
 
-        responseCallback = responseData => {
-          _doSend({
-            handlerName: message.handlerName,
-            responseId: callbackResponseId,
-            responseData,
-          });
-        };
-      }
+      responseCallback(responseData);
 
-      const handler = messageHandlers[message.handlerName];
+      return;
+    }
+
+    /** 原生调JS */
+    if (methodName) {
+      const handler = this.methodHandlers[methodName];
 
       if (!handler) {
-        console.warn('no handler for message from java', message);
-      } else {
-        handler(message.data, responseCallback);
+        console.warn(`methodName ${methodName} 不存在`);
+
+        return;
       }
+
+      handler(data);
     }
   }
-
-  const bridge = {
-    registerHandler(handlerName, handler) {
-      messageHandlers[handlerName] = handler;
-    },
-    callHandler(handlerName, data, responseCallback) {
-      _doSend(
-        {
-          handlerName,
-          data,
-        },
-        responseCallback
-      );
-    },
-    disableJavascriptAlertBoxSafetyTimeout(disable) {
-      this.callHandler('_disableJavascriptAlertBoxSafetyTimeout', disable !== false);
-    },
-    _handleMessageFromJava(messageJSON) {
-      _dispatchMessageFromJava(messageJSON);
-    },
-    hasNativeMethod(name, responseCallback) {
-      this.callHandler('_hasNativeMethod', name, responseCallback);
-    },
-  };
-
-  bridge.registerHandler('_hasJavascriptMethod', (data, responseCallback) => {
-    responseCallback(!!messageHandlers[data]);
-  });
-
-  const callbacks = window.WVJBCallbacks;
-
-  delete window.WVJBCallbacks;
-
-  if (callbacks) {
-    for (let i = 0; i < callbacks.length; i += 1) {
-      callbacks[i](bridge);
-    }
-  }
-
-  window.WebViewJavascriptBridge = bridge;
-})();
-
-export default function initJsBridge() {
-  // eslint-disable-next-line consistent-return
-  function setupWebViewJavascriptBridge(callback) {
-    if (window.WebViewJavascriptBridge) {
-      return callback(window.WebViewJavascriptBridge);
-    }
-
-    if (window.WVJBCallbacks) {
-      return window.WVJBCallbacks.push(callback);
-    }
-
-    window.WVJBCallbacks = [callback];
-
-    const WVJBIframe = document.createElement('iframe');
-
-    WVJBIframe.style.display = 'none';
-    WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
-    document.documentElement.appendChild(WVJBIframe);
-    setTimeout(() => {
-      document.documentElement.removeChild(WVJBIframe);
-    }, 0);
-  }
-
-  // 原生调h5
-  window.bridgeRegisterHandler = function bridgeRegisterHandler(option) {
-    setupWebViewJavascriptBridge(bridge => {
-      // oc调用Js的方法，并且有返回函数   callHeandler(方法名与app端保持一致)  data（IOS传过来的数据）
-      bridge.registerHandler(option.method, (data, responseCallback) => {
-        // js回调函数返回数据
-        if (option.callback) {
-          option.callback(data);
-        }
-
-        const responseData = { success: true };
-
-        responseCallback(responseData, data);
-      });
-    });
-  };
-
-  /**
-   * @description: h5调原生
-   * @param {object} option
-   * @param {string} option.method
-   * @param {object} option.data
-   * @param {function} option.callback
-   * @return {void}
-   */
-  window.callNativePage = function callNativePage(option) {
-    setupWebViewJavascriptBridge(() => {
-      window.WebViewJavascriptBridge.callHandler(option.method, option.data, responseData => {
-        if (option.callback) {
-          option.callback(responseData);
-        }
-      });
-    });
-  };
 }
+
+window.WebViewJavascriptBridge = new JsBridge();
+
+function setupWebViewJavascriptBridge(callback) {
+  if (window.WebViewJavascriptBridge) {
+    callback(window.WebViewJavascriptBridge);
+
+    return;
+  }
+
+  if (window.WVJBCallbacks) {
+    window.WVJBCallbacks.push(callback);
+
+    return;
+  }
+
+  window.WVJBCallbacks = [callback];
+
+  const WVJBIframe = document.createElement('iframe');
+
+  WVJBIframe.style.display = 'none';
+  WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+  document.documentElement.appendChild(WVJBIframe);
+  setTimeout(() => {
+    document.documentElement.removeChild(WVJBIframe);
+  }, 0);
+}
+
+/**
+ * @description: 注册方法给原生
+ * @param {string} option.methodName
+ * @param {function} option.method
+ * @return {*}
+ */
+window.bridgeRegisterHandler = function bridgeRegisterHandler(option) {
+  setupWebViewJavascriptBridge(bridge => {
+    bridge.registerHandler(option.methodName, option.method);
+  });
+};
+
+/**
+ * @description: h5调原生
+ * @param {object} option
+ * @param {string} option.message
+ * @param {function} option.callback
+ * @return {void}
+ */
+window.callNativePage = function callNativePage(option) {
+  setupWebViewJavascriptBridge(bridge => {
+    bridge.callHandler(option.message, responseData => {
+      if (option.callback) {
+        option.callback(responseData);
+      }
+    });
+  });
+};
