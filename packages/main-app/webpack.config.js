35e356be0e0448b6ab947796d6bce31e2dacaad6
吴锦辉 <528935881@qq.com>
2022-07-20 16:15:53 +0800

    feat: webpack方法提取

--- packages/main-app/webpack.config.js
+++ packages/main-app/webpack.config.js
@@ -2,7 +2,7 @@
  * @Description: webpack配置文件
  * @Author: 吴锦辉
  * @Date: 2021-08-16 09:19:32
- * @LastEditTime: 2022-07-20 15:37:31
+ * @LastEditTime: 2022-07-20 16:13:32
  */
 
 const { argv } = require('yargs');
@@ -13,121 +13,20 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const threadLoader = require('thread-loader');
 const PurgeCSSPlugin = require('purgecss-webpack-plugin');
 const glob = require('glob');
-const autoprefixer = require('autoprefixer');
-
-const loaderPoolOptions = {
-  // 池选项，例如传递给 loader 选项
-  // 必须匹配 loader 选项才能启动正确的池
-
-  // 产生的 worker 的数量，默认是 (cpu 核心数 - 1)，或者，
-  // 在 require('os').cpus() 是 undefined 时回退至 1
-  workers: 2,
-
-  // 一个 worker 进程中并行执行工作的数量
-  // 默认为 20
-  workerParallelJobs: 50,
-
-  // 额外的 node.js 参数
-  workerNodeArgs: ['--max-old-space-size=1024'],
-
-  // 允许重新生成一个僵死的 work 池
-  // 这个过程会降低整体编译速度
-  // 并且开发环境应该设置为 false
-  poolRespawn: false,
-
-  // 闲置时定时删除 worker 进程
-  // 默认为 500（ms）
-  // 可以设置为无穷大，这样在监视模式(--watch)下可以保持 worker 持续存在
-  poolTimeout: 2000,
-
-  // 池分配给 worker 的工作数量
-  // 默认为 200
-  // 降低这个数值会降低总体的效率，但是会提升工作分布更均一
-  poolParallelJobs: 50,
-
-  // 池的名称
-  // 可以修改名称来创建其余选项都一样的池
-  name: 'loader-pool',
-};
-
-threadLoader.warmup(loaderPoolOptions, ['babel-loader']);
-
-const babelThreadLoader = {
-  loader: 'thread-loader',
-  options: loaderPoolOptions,
-};
-
-/* node-sass 中有个来自 Node.js 线程池的阻塞线程的 bug。
-  当使用 thread-loader 时，需要设置 workerParallelJobs: 2 */
-const sassLoaderPoolOptions = {
-  ...loaderPoolOptions,
-  workerParallelJobs: 2,
-};
-
-threadLoader.warmup(sassLoaderPoolOptions, ['sass-loader']);
-
-const sassThreadLoader = {
-  loader: 'thread-loader',
-  options: sassLoaderPoolOptions,
-};
-
-const babelLoader = {
-  loader: 'babel-loader',
-  options: {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          useBuiltIns: 'entry',
-          corejs: '3.22',
-        },
-      ],
-      '@babel/preset-react',
-    ],
-  },
-};
-
-const cssLoader = {
-  loader: 'css-loader',
-  options: {
-    importLoaders: 1,
-  },
-};
-
-const postcssLoader = {
-  loader: 'postcss-loader',
-  options: {
-    postcssOptions: {
-      plugins: [autoprefixer()],
-    },
-  },
-};
-
-const sassLoader = {
-  loader: 'sass-loader',
-};
-
-const sassResourcesLoader = {
-  loader: 'sass-resources-loader',
-  options: {
-    sourceMap: true,
-    resources: [path.resolve(__dirname, './src/global.scss')], // 一定是path.resolve的绝对路径
-  },
-};
-
-const imageLoader = limit => ({
-  test: /\.(png|svg|jpg|jpeg|gif)$/,
-  type: 'asset',
-  parser: { dataUrlCondition: { maxSize: limit } },
-});
-
-const fontLoader = {
-  test: /\.(woff|woff2|eot|ttf|otf)$/i,
-  type: 'asset/resource',
-};
+const {
+  setFreeVariable,
+  fontLoader,
+  imageLoader,
+  sassResourcesLoader,
+  sassLoader,
+  postcssLoader,
+  cssLoader,
+  babelLoader,
+  sassThreadLoader,
+  babelThreadLoader,
+} = require('./webpack.part');
 
 const baseConfig = {
   entry: './src/index.js',
@@ -143,31 +42,34 @@ const baseConfig = {
       paths: glob.sync(path.join(__dirname, 'src/**/*'), { nodir: true }),
       only: ['styles'],
     }),
+    setFreeVariable({
+      __name__: 'wujinhui',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [babelThreadLoader, babelLoader],
+        use: [babelThreadLoader(), babelLoader()],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, cssLoader, postcssLoader],
+        use: [MiniCssExtractPlugin.loader, cssLoader(), postcssLoader()],
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          cssLoader,
-          postcssLoader,
-          sassThreadLoader,
-          sassLoader,
-          sassResourcesLoader,
+          cssLoader(),
+          postcssLoader(),
+          sassThreadLoader(),
+          sassLoader(),
+          sassResourcesLoader(['./src/global.scss']),
         ],
       },
       imageLoader(15000),
-      fontLoader,
+      fontLoader(),
     ],
   },
   resolve: {
---
+++ packages/main-app/webpack.part.js
@@ -0,0 +1,168 @@
+/*
+ * @Description: webpack配置方法提取
+ * @Author: 吴锦辉
+ * @Date: 2022-07-20 15:49:03
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-07-20 16:05:04
+ */
+
+const path = require('path');
+const threadLoader = require('thread-loader');
+const autoprefixer = require('autoprefixer');
+const { DefinePlugin } = require('webpack');
+
+const loaderPoolOptions = {
+  // 池选项，例如传递给 loader 选项
+  // 必须匹配 loader 选项才能启动正确的池
+
+  // 产生的 worker 的数量，默认是 (cpu 核心数 - 1)，或者，
+  // 在 require('os').cpus() 是 undefined 时回退至 1
+  workers: 2,
+
+  // 一个 worker 进程中并行执行工作的数量
+  // 默认为 20
+  workerParallelJobs: 50,
+
+  // 额外的 node.js 参数
+  workerNodeArgs: ['--max-old-space-size=1024'],
+
+  // 允许重新生成一个僵死的 work 池
+  // 这个过程会降低整体编译速度
+  // 并且开发环境应该设置为 false
+  poolRespawn: false,
+
+  // 闲置时定时删除 worker 进程
+  // 默认为 500（ms）
+  // 可以设置为无穷大，这样在监视模式(--watch)下可以保持 worker 持续存在
+  poolTimeout: 2000,
+
+  // 池分配给 worker 的工作数量
+  // 默认为 200
+  // 降低这个数值会降低总体的效率，但是会提升工作分布更均一
+  poolParallelJobs: 50,
+
+  // 池的名称
+  // 可以修改名称来创建其余选项都一样的池
+  name: 'loader-pool',
+};
+
+threadLoader.warmup(loaderPoolOptions, ['babel-loader']);
+
+const babelThreadLoader = () => {
+  return {
+    loader: 'thread-loader',
+    options: loaderPoolOptions,
+  };
+};
+
+/* node-sass 中有个来自 Node.js 线程池的阻塞线程的 bug。
+  当使用 thread-loader 时，需要设置 workerParallelJobs: 2 */
+const sassLoaderPoolOptions = {
+  ...loaderPoolOptions,
+  workerParallelJobs: 2,
+};
+
+threadLoader.warmup(sassLoaderPoolOptions, ['sass-loader']);
+
+const sassThreadLoader = () => {
+  return {
+    loader: 'thread-loader',
+    options: sassLoaderPoolOptions,
+  };
+};
+
+const babelLoader = () => {
+  return {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            useBuiltIns: 'entry',
+            corejs: '3.22',
+          },
+        ],
+        '@babel/preset-react',
+      ],
+    },
+  };
+};
+
+const cssLoader = (options = {}) => {
+  return {
+    loader: 'css-loader',
+    options: {
+      importLoaders: 1,
+      ...options,
+    },
+  };
+};
+
+const postcssLoader = () => {
+  return {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        plugins: [autoprefixer()],
+      },
+    },
+  };
+};
+
+const sassLoader = (options = {}) => {
+  return {
+    loader: 'sass-loader',
+    options: {
+      ...options,
+    },
+  };
+};
+
+const sassResourcesLoader = (files = []) => {
+  return {
+    loader: 'sass-resources-loader',
+    options: {
+      sourceMap: true,
+      resources: files.map(v => path.resolve(__dirname, v)),
+    },
+  };
+};
+
+const imageLoader = (limit = 20000) => ({
+  test: /\.(png|svg|jpg|jpeg|gif)$/,
+  type: 'asset',
+  parser: { dataUrlCondition: { maxSize: limit } },
+});
+
+const fontLoader = (options = {}) => {
+  return {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: 'asset/resource',
+    ...options,
+  };
+};
+
+/** 注入变量 */
+const setFreeVariable = (obj = {}) => {
+  const env = {};
+
+  Object.keys(obj).forEach(key => {
+    env[key] = JSON.stringify(obj[key]);
+  });
+
+  return new DefinePlugin(env);
+};
+
+module.exports = {
+  setFreeVariable,
+  fontLoader,
+  imageLoader,
+  sassResourcesLoader,
+  sassLoader,
+  postcssLoader,
+  cssLoader,
+  babelLoader,
+  sassThreadLoader,
+  babelThreadLoader,
+};
